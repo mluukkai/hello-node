7285aa0407a90d76e3c6df243d37c91ef32a0353
Matti Luukkainen <mluukkai@iki.fi>
2023-03-22 09:56:28 +0200

    initial

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
